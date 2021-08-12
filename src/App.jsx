7aac893b3e81fe55587ef7dd29ac5c689f8be31b
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2021-08-12 20:23:51 +0300

    работа с createAsyncThunk

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import InputField from "./components/InputField";
 import TodoList from "./components/TodoList";
 
-import { addTodo } from "./store/todoSlice";
+import { addNewTodo, fetchTodos } from "./store/todoSlice";
 
 import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
+  const { status, error } = useSelector((state) => state.todos);
   const dispatch = useDispatch();
 
   const handleAction = () => {
     if (text.trim().length) {
-      dispatch(addTodo({ text }));
+      dispatch(addNewTodo(text));
       setText("");
     }
   };
 
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <InputField text={text} handleInput={setText} handleSubmit={handleAction} />
+      {status === "loading" && <h2>Loading...</h2>}
+      {error && <h2>An error occured: {error}</h2>}
       <TodoList />
     </div>
   );
